d369283d4b090bea178e5afbe0c676689ce600e6
Adrian Boma <avboma@gmail.com>
2012-11-27 21:56:18 -0500

    initial default rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VilioEmberjsClient::Application.config.secret_token = '4f4e1a18b6da9045d95f23f8b5f075801ec5611d1a67fc81532ae5ec1681129e6f510c98e21ee91aac35042fa5e8526fa3b54f0f28c93e0b32adb562bed51d0a'
