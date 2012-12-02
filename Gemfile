b06606dc5b043e23fb0b3bae6097c905fcd6b30d
Adrian Boma <avboma@gmail.com>
2012-12-01 23:20:59 -0500

    first working version of login

--- Gemfile
+++ Gemfile
@@ -7,13 +7,15 @@ gem 'rails', '3.2.6'
 
 gem 'sqlite3'
 
+group :development, :test do
+  gem 'ruby-debug19'  
+end
 
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
-
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
   # gem 'therubyracer', :platforms => :ruby
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,15 @@
-class V1::SessionsController < ApplicationController
+require 'net/http'
+require 'uri'
+
+class SessionsController < ApplicationController
+  skip_before_filter :verify_auth_token, :only => [:new, :create]
   
   def create
-    user = login(params[:email], params[:password], params[:remember_me])
-    if user
-      redirect_back_or_to root_url, :notice => "Logged in!"
+    #user = login(params[:email], params[:password], params[:remember_me])
+    @auth_token = get_auth_token params
+    if @auth_token
+      session[:auth_token] ||= @auth_token
+      redirect_to root_url, :notice => "Logged in!"
     else
       flash.now.alert = "Email or password was invalid"
       render :new
@@ -11,8 +17,22 @@ class V1::SessionsController < ApplicationController
   end
   
   def destroy
-    logout
-    redirect_to login_url, :alert => "You have been logged out"
+    #logout
+    #redirect_to login_url, :alert => "You have been logged out"
   end
-
+  
+  private 
+  
+  def get_auth_token params
+    net = Net::HTTP.new("localhost", 4444)
+    request = Net::HTTP::Post.new("/sessions")
+    request.body = "{ \"user\" : {\"email\" : \"#{params[:email]}\", \"password\" : \"#{params[:password]}\" }}"
+    request['Content-Type'] = 'application/json'
+    response = net.request(request) 
+    return nil unless response.code == "200"
+    logger.debug "response is a success!"
+    json = JSON.parse(response.body)
+    logger.debug "json is #{json}"
+    return json['session']['auth_token']
+  end
 end
