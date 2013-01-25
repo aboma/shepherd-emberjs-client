c6de2e66154851cda4f52033e1f788a1d3a27719
Adrian Boma <avboma@gmail.com>
2013-01-24 22:57:58 -0500

    fixed portfolio sorting

--- app/assets/javascripts/v1/controllers/portfoliosController.js
+++ app/assets/javascripts/v1/controllers/portfoliosController.js
@@ -1,12 +1,9 @@
 Luxin.PortfoliosController = Ember.ArrayController.extend({
+	needs: ['portfolio'],
 	sortProperties: ['name'],
 	sortAscending: true, 
 	selectedPortfolio: null,
 		
-	initialize : function() {
-		this.connectControllers('portfolio');
-	}, 
-	
 	clearSelected : function() {
 		console.log('clearing selected portfolio');
 		this.set('selectedPortfolio', null);
@@ -15,25 +12,10 @@ Luxin.PortfoliosController = Ember.ArrayController.extend({
 	// set selected portfolio if one is set on the portfolioController
 	observeSelected : function(){
 		console.log('responding to change of port on portController');
-		var port = this.portfolioController.get('content');
-		this.set('selected', port);
+		var port = this.controllerFor('portfolio').get('content');
+		this.set('selectedPortfolio', port);
 	}.observes('portfolioController.content'),
 	
-/*	portfolioNameFilter: '',
-	
-  	filteredPortfolios: function() {
-  		var content = this.get('arrangedContent'),
-  			x = content,
-  			value = this.get('portfolioNameFilter');		
-  		if (value && value.length > 3)
-  			x =  x.filter(function(item, index, enumerable) {
-  				if (item.get('name').toLowerCase().startsWith(value.toLowerCase()))
-  					return true;
-  				return false;
-  			});
-  		return x;
-  	}.property('arrangedContent.@each.name', 'portfolioNameFilter').cacheable(),
- */
   	contentLoaded: function() {
   		console.log('controller content is loaded');
   		// set default value to null so prompt is shown, not first item
