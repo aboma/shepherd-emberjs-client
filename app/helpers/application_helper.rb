38f188032e87def08bde2b2f9f6736413e2f355c
Adrian Boma <avboma@gmail.com>
2013-11-08 13:58:22 -0500

    fix: correct protocol setting to remove colon and slashes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
   def api_path
-    path = SHEPHERD_CONFIG.api.url.host + ':' + SHEPHERD_CONFIG.api.url.port.to_s
-    #path = SHEPHERD_CONFIG.api.url.protocol + '://' + SHEPHERD_CONFIG.api.url.host + ':' + SHEPHERD_CONFIG.api.url.port.to_s
+    path = SHEPHERD_CONFIG.api.url.protocol + '://' + SHEPHERD_CONFIG.api.url.host + ':' + SHEPHERD_CONFIG.api.url.port.to_s
     path = File.join(path, SHEPHERD_CONFIG.api.url.path) if SHEPHERD_CONFIG.api.url.path
     path
   end
